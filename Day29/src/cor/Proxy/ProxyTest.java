dfbdf5e259d6177d7ac1e581549067e73ae9e287
P775Idea <ruicyquan@gmail.com>
2020-03-24 10:03:40 +0800

    day28 白天效率还可以,以后要吃早饭

---
+++ Day29/src/cor/Proxy/ProxyTest.java
@@ -0,0 +1,13 @@
+package cor.Proxy;
+
+/**
+ * version: 1.0
+ *
+ * @author XinLan Wang
+ * @create 2020-03-24 9:52
+ * @description: ProxyTest 类的主要功能为:
+ * 动态代理
+ *
+ */
+public class ProxyTest {
+}
---
+++ Day29/src/cor/Proxy/StaticProxyTest.java
@@ -0,0 +1,43 @@
+package cor.Proxy;
+
+/**
+ * version: 1.0
+ *
+ * @author XinLan Wang
+ * @create 2020-03-24 9:46
+ * @description: StaticProxyTest 类的主要功能为:
+ */
+interface ClothFactory{
+    void produceCloth();
+}
+
+class NikeFactory implements ClothFactory{
+
+    @Override
+    public void produceCloth() {
+        
+    }
+}
+
+class NikeProxyFactory implements ClothFactory{
+    ClothFactory factory;
+
+    public NikeProxyFactory(ClothFactory factory){
+        this.factory = factory;
+    }
+
+    @Override
+    public void produceCloth() {
+        System.out.println("耐克生产前");
+        factory.produceCloth();
+        System.out.println("耐克生产后");
+    }
+}
+
+public class StaticProxyTest{
+
+    public static void main(String[] args) {
+        NikeProxyFactory nikeProxyFactory = new NikeProxyFactory(new NikeFactory());
+        nikeProxyFactory.produceCloth();
+    }
+}
